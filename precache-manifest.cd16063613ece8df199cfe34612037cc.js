b5cb2cc5ad862d01c783fd41e94056398f700cfb
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-04-16 18:35:58 +0200

    Updates

---
+++ precache-manifest.cd16063613ece8df199cfe34612037cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c68035c2502c1ffc2947f220420deb",
+    "url": "/website/static/media/background.35c68035.jpg"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "713d358b84e4f54c2322",
+    "url": "/website/static/js/main.8e34835c.chunk.js"
+  },
+  {
+    "revision": "b88ceb54c35508bb154a",
+    "url": "/website/static/js/2.4e43b36c.chunk.js"
+  },
+  {
+    "revision": "713d358b84e4f54c2322",
+    "url": "/website/static/css/main.eec0712b.chunk.css"
+  },
+  {
+    "revision": "595310a96f67590d41f43d229593fc94",
+    "url": "/website/index.html"
+  }
+];
